5bc4eb4b2a12106e1b38d0e7443a2648d58dc5df
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-05 18:18:58 +0100

    Update OOPBasics.java

--- JAVA/OOPBasics/src/hu/progmatic/OOPBasics.java
+++ JAVA/OOPBasics/src/hu/progmatic/OOPBasics.java
@@ -6,7 +6,7 @@ public class OOPBasics {
 
     public static void main(String[] args) {
         Learner jancsi = new Learner("Jancsi", "1993-01-01", "Kerekerdo utca 1", "+36901234567", "magyar", "festeszet");
-        Learner juliska = new Learner("Jancsi", "1990-07-15", "Csillagtisztas ter 101", "+36807654321", "magyar", "anyagszobraszat");
+        Learner juliska = new Learner("Juliska", "1990-07-15", "Csillagtisztas ter 101", "+36807654321", "magyar", "anyagszobraszat");
 
         System.out.println("Jancsi most " + jancsi.getAge() + " eves");
         System.out.println("Juliska most " + juliska.getAge() + " eves");
