023f49da044293f64c8cb80add9b571175d4bd9f
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-09 19:19:00 +0100

    add java basics

---
+++ JAVA/JavaBasics/src/hu/progmatic/Main.java
@@ -0,0 +1,13 @@
+package hu.progmatic;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String tanfolyam = "Jr. Autom.";
+        int evszam = 2023;
+        String progmatic = "Progmatic";
+
+        evszam = 2024;
+        System.out.print("hello progmatic " + evszam);
+    }
+}
