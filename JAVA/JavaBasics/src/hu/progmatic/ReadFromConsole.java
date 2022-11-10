9a20f06475dacc0de530ed424388872eb7bf5594
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-10 18:53:53 +0100

    add more examples for readfromconsole

--- JAVA/JavaBasics/src/hu/progmatic/ReadFromConsole.java
+++ JAVA/JavaBasics/src/hu/progmatic/ReadFromConsole.java
@@ -7,9 +7,20 @@ public class ReadFromConsole {
     public static void main(String[] args) {
         System.out.println("Give me your name");
 
+        // Ez a sor egyszer kell ha beolvasni szeretnénk
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine(); // a name változó értéke lesz amit beírtunk a console-ra
+
+        /*  az alábbi utasítasok közül válasszuk a nekünk megfelelőt,
+            attól függően milyen értéket szeretnénk beolvasni
+         */
+        // beolvasás a console-ról String-ként
+        String name = scanner.nextLine();
+        // beolvasás a console-ról int-ként
+        int number = scanner.nextInt();
+        // beolvasás a console-ról boolean-ként
+        boolean flag = scanner.nextBoolean();
 
         System.out.println("Hello " + name + "! Good to see you!");
+        System.out.println("I also read " + number + " and " + flag + " from you!");
     }
 }
