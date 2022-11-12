79500701ce51f574f93592794978320f8f588345
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-12 17:09:05 +0100

    Create Task15.java

---
+++ JAVA/JavaBasics/src/Task15.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1st player name:");
+        String player1 = scanner.nextLine();
+        System.out.println("2st player name:");
+        String player2 = scanner.nextLine();
+        int player1Total = 0, player2Total = 0;
+        Random randomGenerator = new Random();
+        int round = 1;
+        while (round <= 3) {
+            int player1ActualNumber = randomGenerator.nextInt(6) + 1;
+            int player2ActualNumber = randomGenerator.nextInt(6) + 1;
+            player1Total = player1Total + player1ActualNumber;
+            player2Total = player2Total + player2ActualNumber;
+            round++;
+        }
+
+        if (player1Total > player2Total) {
+            System.out.println(player1 + " won.");
+        } else if (player2Total > player1Total) {
+            System.out.println(player2 + " won.");
+        } else {
+            System.out.println("Draw.");
+        }
+    }
+}
