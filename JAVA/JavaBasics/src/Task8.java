beb5a78e35c35d5449f136afe694d92b18bc267a
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-12 11:04:36 +0100

    Create Task8.java

---
+++ JAVA/JavaBasics/src/Task8.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Task8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("aoldal:");
+        int aoldal = scanner.nextInt();
+        System.out.println("boldal:");
+        int boldal = scanner.nextInt();
+        System.out.println("coldal:");
+        int coldal = scanner.nextInt();
+
+        if ((aoldal + boldal) > coldal &&
+                (aoldal + coldal) > boldal &&
+                (boldal + coldal) > aoldal) {
+            System.out.println("Triangle");
+        } else {
+            System.out.println("Not a triangle");
+        }
+    }
+}
