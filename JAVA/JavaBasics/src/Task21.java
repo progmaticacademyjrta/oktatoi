6628fbd575fbca6dd6b06d60f87c90e6c6d6fabd
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-14 19:43:35 +0100

    Update Task21.java

--- JAVA/JavaBasics/src/Task21.java
+++ JAVA/JavaBasics/src/Task21.java
@@ -3,19 +3,27 @@ import java.util.Scanner;
 public class Task21 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("aoldal:");
-        int aoldal = scanner.nextInt();
-        System.out.println("boldal:");
-        int boldal = scanner.nextInt();
-        System.out.println("coldal:");
-        int coldal = scanner.nextInt();
+        // FELADAT - ismételje az utasításokat addig a program,
+        // amíg nem lesz szerkeszthető a háromszög
 
-        if ((aoldal + boldal) > coldal &&
-                (aoldal + coldal) > boldal &&
-                (boldal + coldal) > aoldal) {
-            System.out.println("Triangle");
-        } else {
-            System.out.println("Not a triangle");
+        boolean rerun=true;
+        // CIKLUS ELEJE
+        while (rerun) {    // rerun==true
+            System.out.println("aoldal:");
+            int aoldal = scanner.nextInt();
+            System.out.println("boldal:");
+            int boldal = scanner.nextInt();
+            System.out.println("coldal:");
+            int coldal = scanner.nextInt();
+
+            if ((aoldal + boldal) > coldal &&
+                    (aoldal + coldal) > boldal &&
+                    (boldal + coldal) > aoldal) {
+                System.out.println("Triangle");
+            } else {
+                System.out.println("Not a triangle");
+            }
         }
+        // CIKLUS VEGE
     }
 }
