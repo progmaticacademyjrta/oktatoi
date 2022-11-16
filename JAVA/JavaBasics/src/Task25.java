6d21c5061c46bbc09d6068eb20c5636b3250ab64
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-16 19:05:21 +0100

    Update Task25.java

--- JAVA/JavaBasics/src/Task25.java
+++ JAVA/JavaBasics/src/Task25.java
@@ -7,6 +7,7 @@ public class Task25 {
         int dayOfWeek = scanner.nextInt();
 
         // Szelekciók VAGY feltételekkel
+        /*
         if (dayOfWeek == 1 || dayOfWeek == 2 || dayOfWeek == 3 || dayOfWeek == 7) {
             System.out.println("Ez nem a Te napod, majd talán holnap");
         } else if (dayOfWeek == 4 || dayOfWeek == 5 || dayOfWeek == 6) {
@@ -14,7 +15,18 @@ public class Task25 {
         } else {
             System.out.println("Rerun and give number 1-7, please");
         }
+        */
 
         // TASK - rewrite the expression above with the usage of switch-case
+        switch (dayOfWeek) {
+            case 1, 2, 3, 7:
+                System.out.println("Ez nem a Te napod, majd talán holnap");
+                break;
+            case 4, 5, 6:
+                System.out.println("Italos nap van, ide a citromos sört de gyorsan");
+                break;
+            default:
+                System.out.println("Rerun and give number 1-7, please");
+        }
     }
 }
