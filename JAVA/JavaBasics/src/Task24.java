ccee1eabf0caa2aca8385315481631d371507702
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-16 18:44:56 +0100

    add task 24 solution and task 25 skeleton

--- JAVA/JavaBasics/src/Task24.java
+++ JAVA/JavaBasics/src/Task24.java
@@ -7,8 +7,32 @@ public class Task24 {
         String brand = scanner.nextLine();
 
         switch (brand) {
-            case :
+            case "Dreher":
+                System.out.println("Annyi a világ, amennyit beletöltesz");
+                break;
+            case "Halls":
+                System.out.println("Az orrodat is tisztítja");
+                break;
+            case "Gösser":
+                System.out.println("Gut Besser Gösser");
+                break;
+            case "Dunkin Donuts":
+                System.out.println("America runs on dunkin");
+                break;
+            case "Nespresso":
+                System.out.println("What else");
+                break;
+            case "Opel":
+                System.out.println("Sosem kop el");
+                break;
+            case "L'oreal":
+                System.out.println("Mert megérdemlem");
+                break;
+            case "Disneyland":
+                System.out.println("The happiest place on Earth");
+                break;
             default:
+                System.out.println("Not existing brand. Try a new one!");
         }
     }
 }
---
+++ JAVA/JavaBasics/src/Task25.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task25 {
+    public static void main(String[] args) {
+        System.out.println("Which day is today? 1-7");
+        Scanner scanner = new Scanner(System.in);
+        int dayOfWeek = scanner.nextInt();
+
+        // Szelekciók VAGY feltételekkel
+        if (dayOfWeek == 1 || dayOfWeek == 2 || dayOfWeek == 3 || dayOfWeek == 7) {
+            System.out.println("Ez nem a Te napod, majd talán holnap");
+        } else if (dayOfWeek == 4 || dayOfWeek == 5 || dayOfWeek == 6) {
+            System.out.println("Italos nap van, ide a citromos sört de gyorsan");
+        } else {
+            System.out.println("Rerun and give number 1-7, please");
+        }
+    }
+}
