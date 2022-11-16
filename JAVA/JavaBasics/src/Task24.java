08a798e159252da851cd38d69bcadcf3d66db493
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-16 18:28:45 +0100

    Create Task24.java

---
+++ JAVA/JavaBasics/src/Task24.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task24 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give me a brand: ");
+        String brand = scanner.nextLine();
+
+        switch (brand) {
+            case :
+            default:
+        }
+    }
+}
