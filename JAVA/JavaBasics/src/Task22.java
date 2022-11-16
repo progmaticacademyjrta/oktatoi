742bba2d29710c7cb2e0e9ccc7248ff15637835b
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-16 17:28:38 +0100

    Introduce switch-case in task 22

--- JAVA/JavaBasics/src/Task22.java
+++ JAVA/JavaBasics/src/Task22.java
@@ -11,23 +11,30 @@ public class Task22 {
             System.out.println("Give the operator first? (+ - * / pumped)");
             String operator = scan.nextLine();
             System.out.println("First number: ");
-            int number1 = scan.nextInt();
+            int number1 = Integer.parseInt(scan.nextLine());
             System.out.println("Second number: ");
-            int number2 = scan.nextInt();
+            int number2 = Integer.parseInt(scan.nextLine());
 
-            if (operator.equals("*")) {
-                System.out.println("Result: " + number1 * number2);
-            } else if (operator.equals("/")) {
-                System.out.println("Result: " + number1 / number2);
-            } else if (operator.equals("-")) {
-                System.out.println("Result: " + (number1 - number2)); // itt szükség van a zárójelre a műveleti sorrend miatt
-            } else if (operator.equals("+")) {
-                System.out.println("Result: " + (number1 + number2));
-            } else if (operator.equals("pumped")) {
-                System.out.println("Result: " + number1 * number1 * number1 * number2 * number2 * number2);
-            } else {
-                System.out.println("Unsupported operator. Please re-run!");
+            switch (operator) {
+                case "*":
+                    System.out.println("Result: " + number1 * number2);
+                    break;
+                case "/":
+                    System.out.println("Result: " + number1 / number2);
+                    break;
+                case "-":
+                    System.out.println("Result: " + (number1 - number2)); // itt szükség van a zárójelre a műveleti sorrend miatt
+                    break;
+                case "+":
+                    System.out.println("Result: " + (number1 + number2));
+                    break;
+                case "pumped":
+                    System.out.println("Result: " + number1 * number1 * number1 * number2 * number2 * number2);
+                    break;
+                default:
+                    System.out.println("Unsupported operator. Please re-run!");
             }
+
 
             System.out.println("Do you want to re-run? (Y/N)");
             decision = scanRerun.nextLine();
