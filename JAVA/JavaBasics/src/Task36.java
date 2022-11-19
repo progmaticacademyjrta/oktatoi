d5980321ff31fe1f282d728dede1bf47c8b17323
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-19 11:51:17 +0100

    Create Task36.java

---
+++ JAVA/JavaBasics/src/Task36.java
@@ -0,0 +1,13 @@
+public class Task36 {
+    public static void main(String[] args) {
+        int[] eggPrices = {105, 90, 70, 89, 95};
+        double eggPricesSum = 0;
+
+        for (int i = 0; i < eggPrices.length; i++) {
+            eggPricesSum = eggPricesSum + eggPrices[i];    // ua. mint eggPricesSum +=  eggPrices[i]
+        }
+
+        double result = eggPricesSum / eggPrices.length;
+        System.out.println("Average price of an egg: " + result);
+    }
+}
