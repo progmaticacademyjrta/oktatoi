b8bc01b6a5d0fd025b101c06e5e99f18b886a163
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-24 18:53:49 +0100

    Update Task54.java

--- JAVA/JavaBasics/src/Task54.java
+++ JAVA/JavaBasics/src/Task54.java
@@ -11,16 +11,20 @@ public class Task54 {
 
         Scanner scanner = new Scanner(System.in);
         HashMap<String, String> phonebook = new HashMap<>();
-        int counter = 0;
+        //int counter = 0;
 
         do {
             System.out.println("Please add the name of the contact: ");
             String name = scanner.nextLine();
             System.out.println("Please add the number of the contact: ");
             String phoneNumber = scanner.nextLine();
+            if(phonebook.containsKey(name))
+                System.out.println("Phone number updated.");
+            if(phonebook.containsValue(phoneNumber))
+                System.out.println("Phone number exist with someone else.");
             phonebook.put(name, phoneNumber);
-            counter++;
-        } while (counter < 5);
+            //counter++;
+        } while (phonebook.size() < 5);  // vagy while (counter < 5)
 
         System.out.println(phonebook);
     }
