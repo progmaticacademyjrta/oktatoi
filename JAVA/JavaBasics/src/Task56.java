9d93a6829c844cefe15e41acc509050394e7eb06
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-26 09:58:42 +0100

    Create Task56.java

---
+++ JAVA/JavaBasics/src/Task56.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Task56 {
+    public static void main(String[] args) {
+        /**
+         * Készítsetek egy számla végösszeg kalkulátor programot,
+         * amelybe be lehet rögzíteni az adott hónap különböző számláinak az összegét.
+         * A program pedig összeadja őket és kiírja mennyit kell a családi kasszából fizetni összesen.
+         * Magát, hogy milyen számla az amit be kell fizetni, az is kerüljön beolvasásra az összegekkel együtt
+         * (de külön beolvasással - 3 darab -).
+         *
+         * a) Az elemek kerüljenek Mapben mentésre, majd összeadásra és kiíratása.
+         * b) Amennyiben a felhasználó 0 ft-os számlát ír, úgy azt törölje ki a Map-ből a program
+         */
+        Scanner scanner = new Scanner(System.in);
+        HashMap<String, Integer> billsMap = new HashMap<>();
+
+        while (billsMap.size() < 3){
+            System.out.println("Please add the name of the bill: ");
+            String billName = scanner.nextLine();
+            System.out.println("Please add the value of the bill: ");
+            int billValue = Integer.parseInt(scanner.nextLine());
+            billsMap.put(billName,billValue);
+        }
+
+        System.out.println(billsMap);
+    }
+}
