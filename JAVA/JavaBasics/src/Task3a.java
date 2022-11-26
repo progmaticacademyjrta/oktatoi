34e494a9edfaf52f0fb7d4399688edb228c660e5
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-26 15:27:09 +0100

    Create Task3a.java

---
+++ JAVA/JavaBasics/src/Task3a.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task3a {
+    public static void main(String[] args) {
+        System.out.println("Give me a number:");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        System.out.println("Give me another number:");
+        int b = scanner.nextInt();
+
+        int result = (a * 3) * b; /// “a” és “b” szorzata, miután megszoroztuk az “a”-t 3
+        System.out.println("The result is: " + result);
+    }
+}
