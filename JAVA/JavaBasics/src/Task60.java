fba33a86acb81582cf148bf610ab2a99d902ca53
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-28 18:37:27 +0100

    Create Task60.java

---
+++ JAVA/JavaBasics/src/Task60.java
@@ -0,0 +1,9 @@
+public class Task60 {
+    public static void main(String[] args) {
+        printGoodbye();
+    }
+
+    public static void printGoodbye(){
+        System.out.println("Have a nice day, Goodbye!");
+    }
+}
