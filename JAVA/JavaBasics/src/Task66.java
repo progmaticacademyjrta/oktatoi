b5694b593d34717179410a33b399bfc210be5b8e
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-30 19:49:21 +0100

    Create Task66.java

---
+++ JAVA/JavaBasics/src/Task66.java
@@ -0,0 +1,16 @@
+public class Task66 {
+    public static void main(String[] args) {
+        String[] carsArray = {"Kia", "Ferrari", "Bmw", "Alfa Romeo", "Mercedes"};
+        String myFavoriteCar = "Alfa Romeo";
+
+        searchForItemInArray(carsArray,myFavoriteCar);
+
+        for (int i = 0; i < carsArray.length; i++) {
+            if (carsArray[i].equals(myFavoriteCar)) {
+                System.out.println(carsArray[i] + "a kedvenc autómárkám.");
+                break;   // opcionális optimalizálásként, ne fusson tovább a ciklus ha megtalálta a kedvencet
+            }
+            System.out.println("Check: " + i);
+        }
+    }
+}
