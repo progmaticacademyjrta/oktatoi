9f41ddb11cfca368af95f9a555c96d51090705cb
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-11-30 20:25:40 +0100

    add task66

--- JAVA/JavaBasics/src/Task66Improved.java
+++ JAVA/JavaBasics/src/Task66Improved.java
@@ -1,16 +1,22 @@
-public class Task66 {
+public class Task66Improved {
     public static void main(String[] args) {
         String[] carsArray = {"Kia", "Ferrari", "Bmw", "Alfa Romeo", "Mercedes"};
         String myFavoriteCar = "Alfa Romeo";
 
         searchForItemInArray(carsArray, myFavoriteCar);
+
+        /*
+        if (searchForItemInArray(carsArray, myFavoriteCar)) {
+            System.out.println("HURRAY");
+        }
+        */
     }
 
-    public static boolean searchForItemInArray(String[] carsArray, String myFavoriteCar) {
+    public static boolean searchForItemInArray(String[] array, String searchItem) {
         boolean isInArray = false;
-        for (int i = 0; i < carsArray.length; i++) {
-            if (carsArray[i].equals(myFavoriteCar)) {
-                System.out.println(carsArray[i] + " is in the array.");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(searchItem)) {
+                System.out.println(array[i] + " is in the array.");
                 isInArray = true;
                 break;   // opcionális optimalizálásként, ne fusson tovább a ciklus ha megtalálta a kedvencet
             }
