b8e073d5a327e6fced36ad9f5b4b20497f055120
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-03 17:00:32 +0100

    Create Task80.java

---
+++ JAVA/JavaBasics/src/Task80.java
@@ -0,0 +1,17 @@
+public class Task80 {
+    /**
+     * Írjatok egy metódust, amely képes megkeresni a paraméterként kapott szöveg elejéről,
+     * a szintén paraméterként kapott darabszámig kivágni a karaktereket.
+     *
+     * Az eredményStringet adja vissza a metódus.
+     * Parancssori argumentumként feldolgozott értékekkel, futassátok meg a metódust a main-ből.
+     * @param args
+     */
+    public static void main(String[] args) {
+        getSubStringofString(args[0],args[1]);
+    }
+
+    public static ??? getSubStringofString(???, ???){
+
+    }
+}
