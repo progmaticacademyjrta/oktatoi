077d83431c26fc44ab7791b1412aceda12b49c60
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-03 17:47:46 +0100

    Update Task82.java

--- JAVA/JavaBasics/src/Task82.java
+++ JAVA/JavaBasics/src/Task82.java
@@ -3,11 +3,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Task82 {
+    /**
+     * Írjatok családbarát metódust, amely egy,
+     * a metódusban előre létrehozott listában tárolt szavazat megkeres egyesével
+     * a paraméterként kapott szövegben.
+     *
+     * a) Amennyiben megtalálja, cserélje ezeket “*” karakterekre.
+     * b) A csillagozott rész legyen pont annyi karakter, amennyi az eredeti szó volt
+     */
     public static void main(String[] args) {
-        //List<String> words =  Arrays.asList("kaki", "mégcsúnyábbkaki");
-        String wrongWord = "kaki";
-        String text = "Elmentem a boltba de találkoztam azzal a kaki bácsival.";
+        List<String> words =  Arrays.asList("kaki", "mégcsúnyábbkaki");
+        //String wrongWord = "kaki";
+        String text = "Elmentem abba a kaki boltba de találkoztam azzal a kaki bácsival.";
 
-        String resultText = "Elmentem a boltba de találkoztam azzal a * bácsival.";
+        String resultText = text.replaceAll(wrongWord,"*");
+
+        System.out.println(resultText);
     }
 }
