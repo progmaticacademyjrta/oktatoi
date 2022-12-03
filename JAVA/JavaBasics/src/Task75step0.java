cacee715abdc4dbc17c999775c8cb89d8bc144f5
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-03 13:35:30 +0100

    Create Task75step0.java

---
+++ JAVA/JavaBasics/src/Task75step0.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Task75step0 {
+    public static void main(String[] args) {
+        List<String> testruns = Arrays.asList("00171", "0018X");
+        String testrun = testruns.get(0); //testrun = 00171
+
+    }
+}
