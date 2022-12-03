84fc8898a0e2df51b57b75ca6c58e0f06a1bf6f4
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-03 17:34:16 +0100

    Create Task82.java

---
+++ JAVA/JavaBasics/src/Task82.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Task82 {
+    public static void main(String[] args) {
+        //List<String> words =  Arrays.asList("kaki", "mégcsúnyábbkaki");
+        String wrongWord = "kaki";
+        String text = "Elmentem a boltba de találkoztam azzal a kaki bácsival.";
+
+        String resultText = "Elmentem a boltba de találkoztam azzal a * bácsival.";
+    }
+}
