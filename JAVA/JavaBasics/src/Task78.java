6964aa992d3a016d68f6648b8192f7ff57f06e07
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-03 15:18:09 +0100

    Create Task78.java

---
+++ JAVA/JavaBasics/src/Task78.java
@@ -0,0 +1,17 @@
+public class Task78 {
+    /**
+     * Írjatok egy metódust a paraméterként kapott szövegről,
+     * illetve a másik paraméterként kapott kifejezésről eldönti,
+     * hogy azzal a kifejezéssel kezdődik-e az adott szöveg vagy sem.
+     *
+     * Amennyiben igen, térjen vissza true-val, egyébként false-al.
+     *
+     * a) hogy ki tudjuk próbálni a metódust, konzolról olvassunk be két értéket a main metódusban és hívjuk meg a készített metódust velük.
+     * b) parancssori argumentumként olvassunk be két értéket a main metódusban és hívjuk meg a készített metódust velük.
+     * c) biztosítsátok be a metódust, hogy nem került a szövegbe szóköz.
+     */
+    public static void main(String[] args) {
+        isStringStartWithText()
+    }
+
+}
