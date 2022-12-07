e60ced36c3cc9913c6311776e8e88ddb954b7377
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-07 19:42:56 +0100

    Create Task84.java

---
+++ JAVA/JavaBasics/src/Task84.java
@@ -0,0 +1,9 @@
+public class Task84 {
+    public static void main(String[] args) {
+        printArgConsole(args[0]);
+    }
+
+    public static void printArgConsole(String value){
+        System.out.println(value);
+    }
+}
