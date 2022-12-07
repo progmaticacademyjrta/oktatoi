81149cd1ebf78bed4c98d90185aa31ab86ea5adf
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-07 17:23:01 +0100

    Update Task83.java

--- JAVA/JavaBasics/src/Task83.java
+++ JAVA/JavaBasics/src/Task83.java
@@ -52,11 +52,11 @@ public class Task83 {
         );
 
         for (String vitaminConsumption :vitaminConsumptions) {
-            String[] values = ??
-            String name = ??
-            int vitaminCValue = ??
-            int vitaminDValue = ??
-            int vitaminEValue = ??
+            String[] values = ??;
+            String name = ??;
+            int vitaminCValue = ??;
+            int vitaminDValue = ??;
+            int vitaminEValue = ??;
         }
 
     }
