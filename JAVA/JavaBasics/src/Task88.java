33f3dbf866a10e0f62745a20ab163342591169c1
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-08 19:40:59 +0100

    Create Task88.java

---
+++ JAVA/JavaBasics/src/Task88.java
@@ -0,0 +1,11 @@
+public class Task88 {
+    /**
+     * Készítsetek programot, amely beolvas egy híres sportolóat tartalmazó fájl,
+     * majd megvizsgálja, hogy a parancssori argumentumként megadott sportolót tartalmazza-e.
+     * Ha igen, írja ki hogy “It’s in the file”,
+     * egyébként pedig írja ki, hogy “No luck” a konzolra.
+     */
+    public static void main(String[] args) {
+
+    }
+}
