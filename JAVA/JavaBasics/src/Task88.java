7f85555ae818ae2c9c03a1586eec62730cb179a0
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-08 20:00:16 +0100

    add task88 solution

--- JAVA/JavaBasics/src/Task88.java
+++ JAVA/JavaBasics/src/Task88.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task88 {
     /**
      * Készítsetek programot, amely beolvas egy híres sportolóat tartalmazó fájl,
@@ -7,5 +13,21 @@ public class Task88 {
      */
     public static void main(String[] args) {
 
+        List<String> sportNamesList = new ArrayList<>();
+        try {
+            sportNamesList = Files.readAllLines(Path.of("src/resources/sport-names.txt"));
+        } catch (IOException e) {
+            System.out.println("Could find the file! :-( ");
+        }
+        System.out.println("File data:" + sportNamesList);
+
+        try {
+            if (sportNamesList.contains(args[0]))
+                System.out.println("It’s in the file");
+            else
+                System.out.println("No luck");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("There is no program arguments added.");
+        }
     }
 }
