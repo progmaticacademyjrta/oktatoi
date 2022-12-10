40bb1ecac7fec2cf4798813ba71a0ce9df4ada94
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-10 11:08:03 +0100

    Update Task90.java

--- JAVA/JavaBasics/src/Task90.java
+++ JAVA/JavaBasics/src/Task90.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -23,19 +26,25 @@ public class Task90 {
             shoppingList.add(item);
         } while (shoppingList.size() != 3);
 
+        // 1) menjünk végig a listán és minden elem esetén menjtsük el / fűzzük hozzá azt egy String változóba
         String content = "";
         for (String shoppingItem : shoppingList) {
             //shoppingItem = "tej 3db";
             content += shoppingItem + System.lineSeparator();
         }
 
-        // 1) menjünk végig a listán és minden elem esetén menjtsük el / fűzzük hozzá azt egy String változóba
         writeShoppingListToFile(content);
     }
 
     public static void writeShoppingListToFile(String item) {
         System.out.println("I will save: " + System.lineSeparator() + item + "to the file.");
-        // 2) fájlba írás elkíszítése
+        // 2) fájlba írás elkészítése
+        try {
+            Files.writeString(Path.of("src/resources/shoppinglist.txt"), item);
+            System.out.println("File saved.");
+        } catch (IOException e) {
+            System.out.println("Couldn't save the following file: " + e.getMessage());
+        }
     }
 
 }
