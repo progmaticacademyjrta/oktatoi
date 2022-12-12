94754c11b4f21ac170a591b4d1efc4c17238e018
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-12 19:52:18 +0100

    Create Donut.java

---
+++ JAVA/JavaCore/src/Donut.java
@@ -0,0 +1,57 @@
+public class Donut {
+    private String shape;
+    private int size;
+    private String flavor;
+    private String ingredients;
+    private String decor;
+
+    public Donut(String shape, int size, String flavor, String ingredients, String decor) {
+        this.shape = shape;
+        this.size = size;
+        this.flavor = flavor;
+        this.ingredients = ingredients;
+        this.decor = decor;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getDecor() {
+        return decor;
+    }
+
+    public void setDecor(String decor) {
+        this.decor = decor;
+    }
+
+
+}
