6adbab8d2c238f863ebccf357445487d4e6bfb56
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-12 19:13:25 +0100

    Create AutoMain.java

---
+++ JAVA/JavaCore/src/AutoMain.java
@@ -0,0 +1,11 @@
+public class AutoMain {
+    public static void main(String[] args) {
+        Auto toyotaYaris = new Auto("Toyota","Yaris",2020,5,"kolbászzsírmetál");
+        Auto alfagt = new Auto("Alfa Romeo","GT",2006,3,"fekete");
+        Auto scania = new Auto("Scania","R500 super",2021,2,"tűzpiros");
+
+        System.out.println(toyotaYaris);
+        System.out.println(alfagt);
+        System.out.println(scania);
+    }
+}
