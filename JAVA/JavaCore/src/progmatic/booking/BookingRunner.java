ac99289b0386aff1600ca4caebacff289898e2b0
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-14 22:00:54 +0100

    Update BookingRunner.java

--- JAVA/JavaCore/src/progmatic/booking/BookingRunner.java
+++ JAVA/JavaCore/src/progmatic/booking/BookingRunner.java
@@ -5,27 +5,31 @@ import java.util.Scanner;
 public class BookingRunner {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("Name:");
-        String name = scn.nextLine();
-        System.out.println("Phone number:");
-        int phone;
-        try {
-            phone = Integer.parseInt(scn.nextLine());
-        } catch (NumberFormatException ex) {
-            phone = 0;
+
+        int bookingCount = Integer.parseInt(args[0]);
+        for (int i = 0; i < bookingCount; i++) {
+
+            System.out.println("Name:");
+            String name = scn.nextLine();
+            System.out.println("Phone number:");
+            int phone;
+            try {
+                phone = Integer.parseInt(scn.nextLine());
+            } catch (NumberFormatException ex) {
+                phone = 0;
+            }
+            System.out.println("Check in date:");
+            String checkin = scn.nextLine();
+            System.out.println("Check out date:");
+            String checkout = scn.nextLine();
+            System.out.println("Number of guests:");
+            int guests = Integer.parseInt(scn.nextLine());
+
+            Booking booking = new Booking(name, checkin, checkout, guests);
+            if (phone != 0)
+                booking.setPhoneNumber(phone);
+
+            System.out.println(booking);
         }
-        System.out.println("Check in date:");
-        String checkin = scn.nextLine();
-        System.out.println("Check out date:");
-        String checkout = scn.nextLine();
-        System.out.println("Number of guests:");
-        int guests = Integer.parseInt(scn.nextLine());
-
-        Booking kerry = new Booking(name, checkin, checkout, guests);
-        if (phone != 0)
-            kerry.setPhoneNumber(phone);
-
-        System.out.println(kerry);
-
     }
 }
