1e9149fe3eee570d631c38e51a207ff9ecb6989f
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-19 17:36:07 +0100

    add skeleton

---
+++ JAVA/JavaCore/src/progmatic/racing/FormulaOneRunner.java
@@ -0,0 +1,7 @@
+package progmatic.racing;
+
+public class FormulaOneRunner {
+    public static void main(String[] args) {
+        MercedesPilotOne me
+    }
+}
---
+++ JAVA/JavaCore/src/progmatic/racing/MercedesPilotOne.java
@@ -0,0 +1,25 @@
+package progmatic.racing;
+
+public class MercedesPilotOne implements Car,Racer{
+
+
+    @Override
+    public void acceleration(double time) {
+
+    }
+
+    @Override
+    public String getCarManufacturer() {
+        return null;
+    }
+
+    @Override
+    public String getRacerName() {
+        return null;
+    }
+
+    @Override
+    public String getNationality() {
+        return null;
+    }
+}
