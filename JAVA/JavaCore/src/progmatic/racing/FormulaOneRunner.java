d76f9d0178b710b773f11d57f77fe30808b427dd
zsszurovecz <zsolt.szurovecz@getbridge.com>
2022-12-19 18:00:13 +0100

    add 13a solution

--- JAVA/JavaCore/src/progmatic/racing/FormulaOneRunner.java
+++ JAVA/JavaCore/src/progmatic/racing/FormulaOneRunner.java
@@ -2,6 +2,11 @@ package progmatic.racing;
 
 public class FormulaOneRunner {
     public static void main(String[] args) {
-        MercedesPilotOne me
+        MercedesPilotOne hamilton = new MercedesPilotOne("Hamilton","brit","Mercedes");
+        hamilton.acceleration(2.6);
+        System.out.println(hamilton.getCarManufacturer());
+        System.out.println(hamilton.getNationality());
+        System.out.println(hamilton.getRacerName());
+        System.out.println(hamilton);
     }
 }
--- JAVA/JavaCore/src/progmatic/racing/MercedesPilotOne.java
+++ JAVA/JavaCore/src/progmatic/racing/MercedesPilotOne.java
@@ -1,25 +1,44 @@
 package progmatic.racing;
 
 public class MercedesPilotOne implements Car,Racer{
+    private String racerName;
+    private String nationality;
+    private String manufacturer;
+    private double acceleration;
 
+    MercedesPilotOne(String racerName, String nationality, String manufacturer){
+        this.racerName  = racerName;
+        this.nationality  = nationality;
+        this.manufacturer  = manufacturer;
+    }
 
     @Override
     public void acceleration(double time) {
-
+        acceleration=time;
     }
 
     @Override
     public String getCarManufacturer() {
-        return null;
+        return manufacturer;
     }
 
     @Override
     public String getRacerName() {
-        return null;
+        return racerName;
     }
 
     @Override
     public String getNationality() {
-        return null;
+        return nationality;
+    }
+
+    @Override
+    public String toString() {
+        return "MercedesPilotOne{" +
+                "racerName='" + racerName + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", acceleration=" + acceleration +
+                '}';
     }
 }
