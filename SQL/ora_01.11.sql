2b74ef975b81c206b390292a6b4793d6b9318d48
zsszurovecz <zsolt.szurovecz@getbridge.com>
2023-01-11 18:06:12 +0100

    Create ora_01.11.sql

---
+++ SQL/ora_01.11.sql
@@ -0,0 +1,20 @@
+SELECT csoport_neve FROM csoport;
+SELECT * from csoport;
+SELECT * FROM csoport WHERE atlag_eletkor > 60;
+SELECT * FROM csoport WHERE atlag_eletkor > 30 AND pontszam >  200 ;
+
+SELECT * from csoport ORDER BY atlag_eletkor DESC;
+SELECT * from csoport ORDER BY csoport_neve, atlag_eletkor DESC;
+
+SELECT * from csoport LIMIT 2;
+
+SELECT csoport_neve, nemzetiseg, pontszam, atlag_eletkor FROM csoport WHERE atlag_eletkor > 18 ORDER BY csoport_neve;
+
+SELECT COUNT(nemzetiseg) FROM csoport;
+SELECT COUNT(nemzetiseg) FROM csoport WHERE atlag_eletkor > 18;
+
+SELECT MAX(atlag_eletkor) FROM csoport;
+
+SELECT COUNT(csoport_neve) FROM csoport GROUP BY atlag_eletkor;
+SELECT COUNT(csoport_neve),atlag_eletkor FROM csoport GROUP BY atlag_eletkor;
+SELECT COUNT(csoport_neve),atlag_eletkor FROM csoport GROUP BY atlag_eletkor HAVING atlag_eletkor > 18;;
